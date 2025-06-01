35a0b40344f8c1ff5274e3b41b0b38646dc890ed
Rodwan Barbier <rodwanb@gmail.com>
2025-06-01 20:10:49 +0200

    Updates

--- types/hymn.ts
+++
@@ -1,8 +0,0 @@
-export type Hymn = {
-  id: number;
-  category: string;
-  category_id: number;
-  name: string;
-  verses: { text: string }[];
-  file_name?: string; // Optional field for audio file name
-};
